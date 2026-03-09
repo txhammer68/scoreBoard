bd31f284de5528dd4f7da12857de7cfa4bc1df9b
TxHammer <txhammer68@users.noreply.github.com>
2026-03-08 19:56:08 -0500

    Delete contents/ui/scoresAPI.js

--- contents/ui/scoresAPI.js
+++
@@ -1,9 +0,0 @@
-var urls=[
-  "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard",
-  "https://site.api.espn.com/apis/site/v2/sports/soccer/usa.1/scoreboard",
-  "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard",
-  "https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard",
-  "https://site.api.espn.com/apis/site/v2/sports/hockey/nhl/scoreboard",
-  "https://site.api.espn.com/apis/site/v2/sports/basketball/wnba/scoreboard",
-  "https://site.api.espn.com/apis/site/v2/sports/soccer/fifa.world/scoreboard"
-  ]
